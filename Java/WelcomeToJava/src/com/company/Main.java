ae399f3cab885cc087776621808642646795e328
DaoDucDev <daoducdev@gmail.com>
2020-02-03 17:50:19 +0700

    Khai bao bien

--- Java/WelcomeToJava/src/com/company/Main.java
+++ Java/WelcomeToJava/src/com/company/Main.java
@@ -5,5 +5,11 @@ public class Main {
     public static void main(String[] args) {
         //In ra man hinh dong chu Chao mung den voi ngon ngu lap trinh Java!
         System.out.printf("Chao mung den voi ngon ngu lap trinh Java!");
+
+        //Khai bao bien
+        int count;
+        double radius;
+        char kiTu;
+        char number = 10;
     }
 }
